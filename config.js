fc2910b53f27bea3731c6fed0e6efaf19f44ed93
jasoeer <jonayedabdurrahman1@gmail.com>
2024-08-15 15:36:42 +0600

    Update config.js

--- config.js
+++ config.js
@@ -40,12 +40,9 @@ module.exports = {
 
   nodes: [
     {
-        name: "Node #1",
-        host: "lava-v3.ajieblogs.eu.org",
-        port: 80,
-        password: "https://dsc.gg/ajidevserver",
-        reconnectTimeout: 5000,
-        reconnectTries: Infinity,
+        host: "37.114.42.191",
+        port: 6767,
+        password: "danteisnttaken",
         secure: false
     },
  ]
